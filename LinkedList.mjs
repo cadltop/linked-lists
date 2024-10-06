7af2f082924edec3085f578c10657edcfd4fe825
cadltop <caesarfromthecross.808@gmail.com>
2024-10-06 02:24:37 -0500

    Set head and tail methods

--- LinkedList.mjs
+++ LinkedList.mjs
@@ -1,12 +1,13 @@
 import Node from "./Node.mjs";
 export default class {
     nodes = null;
-
+    size = 0;
     prepend(value) {
         const node = new Node();
         node.value = value;
         node.nextNode = this.nodes;
         this.nodes = node;
+        this.size++;
     }
     append(value) {
         if(this.nodes === null)  this.prepend(value)
@@ -17,5 +18,14 @@ export default class {
             node.value = value;
             currentNode.next = node;
         }
+        this.size++;
+    }
+    head() {
+        return this.nodes.value;
+    }
+    tail() {
+        let currentNode = this.nodes;
+        while(currentNode.next !== null) currentNode = currentNode.next;
+        return currentNode.value;
     }
 }

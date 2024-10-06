a71a96806af343a2501b1fc149cf0ccc18af4898
cadltop <caesarfromthecross.808@gmail.com>
2024-10-06 02:07:33 -0500

    Refactor append and prepend methods in Linked List

--- LinkedList.mjs
+++ LinkedList.mjs
@@ -1,3 +1,21 @@
-export class LinkedList {
-    
+import Node from "./Node.mjs";
+export default class {
+    nodes = null;
+
+    prepend(value) {
+        const node = new Node();
+        node.value = value;
+        node.nextNode = this.nodes;
+        this.nodes = node;
+    }
+    append(value) {
+        if(this.nodes === null)  this.prepend(value)
+        else {
+            let currentNode = this.nodes;
+            while(currentNode.next !== null) currentNode = currentNode.next;
+            const node = new Node();
+            node.value = value;
+            currentNode.next = node;
+        }
+    }
 }

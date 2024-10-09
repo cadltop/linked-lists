9ece19b132bf9afae0773fa0b92b0920767c14dd
cadltop <caesarfromthecross.808@gmail.com>
2024-10-08 23:59:48 -0500

    Fix method errors and make nodes private

--- LinkedList.mjs
+++ LinkedList.mjs
@@ -1,19 +1,19 @@
 import Node from "./Node.mjs";
 export default class {
-    nodes = null;
+    #nodes = null;
     size = 0;
 
     prepend(value) {
         const node = new Node();
         node.value = value;
-        node.nextNode = this.nodes;
-        this.nodes = node;
+        node.nextNode = this.#nodes;
+        this.#nodes = node;
         this.size++;
     }
     append(value) {
-        if(this.nodes === null) this.prepend(value);
+        if(this.#nodes === null) this.prepend(value);
         else {
-            let currentNode = this.nodes;
+            let currentNode = this.#nodes;
             while(currentNode.nextNode !== null) currentNode = currentNode.nextNode;
             const node = new Node();
             node.value = value;
@@ -23,77 +23,74 @@ export default class {
     }
 
     head() {
-        return this.nodes.value;
+        return this.#nodes.value;
     }
     tail() {
-        let currentNode = this.nodes;
+        let currentNode = this.#nodes;
         while(currentNode.nextNode !== null) currentNode = currentNode.nextNode;
         return currentNode.value;
     }
 
     at(index) {
-        let currentNode = this.nodes;
+        let currentNode = this.#nodes;
         for (let i = 0; i < index; i++) {
             currentNode = currentNode.nextNode;
         }
         return currentNode.value;
     }
     pop() {
-        let currentNode = this.nodes;
-        for (let i = 1; i < this.size; i++) {
-            if (i !== (this.size - 1)) currentNode = currentNode.nextNode;
-            else currentNode.nextNode = null;
-        }
+        let currentNode = this.#nodes;
+        while(currentNode.nextNode.nextNode !== null) currentNode = currentNode.nextNode;
+        currentNode.nextNode = null;
+        this.size--;
     }
 
     contains(value) {
-        let currentNode = this.nodes;
-        while(currentNode.nextNode !== null) {
+        let currentNode = this.#nodes;
+        for (let i = 0; i < this.size; i++) {
             if (currentNode.value === value) return true;
+            else if (currentNode.nextNode === null) return false;
             else currentNode = currentNode.nextNode;
         }
-        return false;
     }
     find(value) {
-        let currentNode = this.nodes;
-        for (let i = 1; i < this.size; i++) {
+        let currentNode = this.#nodes;
+        for (let i = 0; i < this.size; i++) {
             if (currentNode.value === value) return i;
+            else if (currentNode.nextNode === null) return null;
             else currentNode = currentNode.nextNode;
         }
-        return null;
     }
     
     toString() {
-        let currentNode = this.nodes;
+        let currentNode = this.#nodes;
         let string = "";
-        for (let i = 1; i < this.size; i++) {
-            if (currentNode.nextNode) {
+        for (let i = 0; i < this.size; i++) {
+            if (currentNode.nextNode !== null) {
                 string = string + ` ( ${currentNode.value} ) ->`;
                 currentNode = currentNode.nextNode;
             } else {
                 string = string + ` ( ${currentNode.value} ) -> null`;
+                return string;
             }
         }
-        return string;
     }
     insertAt(value, index) {
-        let currentNode = this.nodes;
-        for (let i = 1; i <= index; i++) {
-            if (i === index) {
+        let currentNode = this.#nodes;
+        for (let i = 0; i < index; i++) {
+            if (i === (index - 1)) {
                 const node = new Node();
                 node.value = value;
                 node.nextNode = currentNode.nextNode;
                 currentNode.nextNode = node;
             } else currentNode = currentNode.nextNode;
         }
-        return string;
     }
     removeAt(index) {
-        let currentNode = this.nodes;
-        for (let i = 1; i <= index; i++) {
-            if (i === index) currentNode.nextNode = currentNode.nextNode.nexnode;
+        let currentNode = this.#nodes;
+        for (let i = 0; i < index; i++) {
+            if (i === (index - 1)) currentNode.nextNode = currentNode.nextNode.nextNode;
             else currentNode = currentNode.nextNode;
         }
-        return string;
     }
 }
